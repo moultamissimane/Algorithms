26c939a945722c195a06c6e16af7b5e2a060181b
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2022-03-10 11:09:16 +0100

    add changes in graph.js

--- graph.js
+++ graph.js
@@ -1,5 +1,5 @@
 // undirected, unweighted graph implementation using adjacency list
-
+// adjacency means that two vertices are said to be adjacent if there is an edge connecting them directly.
 class Graph {
     constructor() {
       this.adjacencyList = {};
@@ -33,3 +33,43 @@ class Graph {
       delete this.adjacencyList[vertex];
     }  
   }
+
+const airports = "PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM".split(" ");
+
+const routes = [
+    ["PHX", "LAX"],
+    ["PHX", "JFK"],
+    ["JFK", "OKH"],
+    ["JFK", "HEL"],
+    ["JFK", "LOS"],
+    ["MEX", "LAX"],
+    ["MEX", "BKK"],
+    ["MEX", "LIM"],
+    ["MEX", "EZE"],
+    ["LIM", "BKK"]
+];
+
+const adjacencyList = new Map();
+
+function addNode(airport) {
+    adjacencyList.set(airport, []);
+}
+
+function addEdge(origin, destination) {
+    adjacencyList.get(origin).push(destination);
+    const dest = adjacencyList.get(destination);
+
+    if (!dest) {
+        addNode(destination)
+    }
+    adjacencyList.get(destination).push(origin);
+}
+
+airports.forEach(addNode);
+
+routes.forEach(route => addEdge(...route));
+
+console.log("result");
+adjacencyList.forEach((value, key) => {
+    console.log(key, value);
+});
